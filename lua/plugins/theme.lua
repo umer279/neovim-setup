2dc7396a2c376b0de422109a1db1a7fde2d4f329
umer <umer.shahzaddar@subbyx.com>
2026-03-10 13:20:30 +0100

    updated theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,15 +1,15 @@
-return { 
-  "catppuccin/nvim", 
-  name = "catppuccin", 
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-     flavour = "frappe",
-     transparent_background = true,
-     float = {
-        transparent = true,
-    },
-    })
-	  vim.cmd.colorscheme "catppuccin"
-  end,
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "frappe",
+			transparent_background = false,
+			float = {
+				transparent = false,
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
